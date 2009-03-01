9852f7f91d4bc90c5109c190031ce12e4aae2e07
mathieul <mathieul@gmail.com>
2009-02-28 21:00:26 -0800

    added validates_email_format_of plugin

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,14 +2,8 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  include AuthenticatedSystem
   helper :all # include all helpers, all the time
-
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '916aa3b2e7c5ba20c0d3ef5435c6a14d'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,15 +17,13 @@ class User < ActiveRecord::Base
   validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
   validates_uniqueness_of   :email
-  validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
+  validates_email_format_of :email
 
-  
 
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
   attr_accessible :login, :email, :name, :password, :password_confirmation
-
 
 
   # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
