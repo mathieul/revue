d1379cf9a33f1e46cedd5b4460d1ff6eaeb646f7
mathieul <mathieul@gmail.com>
2009-03-01 23:19:18 -0800

    added segmented button images, css styles and helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
     options = args.extract_options!.merge(:builder => LabeledFormBuilder)
     form_for(*(args.push(options)), &block)
   end
+  
+  def segmented_button(label, url, active, position)
+    class_names = active == :active ? 'button active-' : 'button unactive-'
+    class_names += position.to_s
+    link_to(content_tag(:span, label), url, :class => class_names)
+  end
 end
